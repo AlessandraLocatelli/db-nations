a4b309efaba7a0989aa0f260e26b92fb7fbdfbf6
Ales <alocatelli798@gmail.com>
2023-09-11 10:46:33 +0200

    refactoring done in class.

--- src/main/java/org/java/nations/Main.java
+++ src/main/java/org/java/nations/Main.java
@@ -10,45 +10,44 @@ public class Main {
     private final static String DB_PASSWORD = "Root_root26.";
 
 
-    private static final String SQL_COUNTRIES = "SELECT countries.country_id as id, countries.name as Country, regions.name as Region, continents.name as Continent\n" +
-            "FROM countries \n" +
-            "JOIN regions on regions.region_id = countries.region_id\n" +
-            "JOIN continents on continents.continent_id = regions.continent_id\n" +
-            "WHERE countries.name LIKE ? "+
-            "ORDER BY country; ";
+    private static final String SQL_COUNTRIES = """
+             SELECT countries.country_id as id, countries.name as Country, regions.name as Region, continents.name as Continent
+             FROM countries 
+             JOIN regions on regions.region_id = countries.region_id 
+             JOIN continents on continents.continent_id = regions.continent_id 
+             WHERE countries.name LIKE ? 
+             ORDER BY countries.name;
+             """;
 
-    private static final String SQL_COUNTRIES_ID = "SELECT c.name as Country, \n" +
-            " l.`language` as Languages, \n" +
-            "cs.`year` as `Year`, cs.population as Population, \n" +
-            "cs.gdp as GDP \n" +
-            "FROM countries c\n" +
-            "JOIN country_stats cs on cs.country_id = c.country_id \n" +
-            "JOIN country_languages cl on cl.country_id = c.country_id \n" +
-            "JOIN languages l on l.language_id = cl.language_id \n" +
-            "WHERE c.country_id = ?; ";
-
+    private static final String SQL_COUNTRY_STAT = """
+                         SELECT c.name as Country,
+                         l.`language` as Languages,
+                         cs.`year` as `Year`, 
+                         cs.population as Population,
+                         cs.gdp as GDP 
+                         FROM countries c
+                         JOIN country_stats cs on cs.country_id = c.country_id
+                         JOIN country_languages cl on cl.country_id = c.country_id
+                         JOIN languages l on l.language_id = cl.language_id
+                         WHERE c.country_id = ?;
+                          """;
 
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-    try(Connection con = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD))
-    {
-
-        System.out.println("Insert a string to look for Countries: ");
+        System.out.println("Search: ");
         String userString = sc.nextLine();
 
+    try(Connection con = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD)) {
 
-        try(PreparedStatement ps = con.prepareStatement(SQL_COUNTRIES))
-        {
 
-            ps.setString(1,"%"+userString+"%");
+        try (PreparedStatement ps = con.prepareStatement(SQL_COUNTRIES)) {
 
-            try(ResultSet rs = ps.executeQuery())
-            {
-                while(rs.next())
-                {
+            ps.setString(1, "%" + userString + "%");
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
                     int id = rs.getInt("id");
                     String country = rs.getString("Country");
                     String region = rs.getString("Region");
@@ -62,27 +61,35 @@ public class Main {
                     System.out.print("continent: ");
                     System.out.println(continent);
                     System.out.println();
-
-
                 }
 
             }
+            catch (SQLException e)
+            {
+                System.out.println("Unable to execute statement.");
+                e.printStackTrace();
 
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Unable to prepare statement.");
+            e.printStackTrace();
         }
 
-        System.out.println("Insert the ID of the Country from which you want to retrieve all the datas: ");
+        System.out.println("Insert the ID of the Country from which you want to retrieve all the data: ");
         int userIDChoice = Integer.parseInt(sc.nextLine());
 
-        try(PreparedStatement ps = con.prepareStatement(SQL_COUNTRIES_ID))
+        try(PreparedStatement ps = con.prepareStatement(SQL_COUNTRY_STAT))
         {
             ps.setInt(1, userIDChoice);
 
            try(ResultSet rs = ps.executeQuery())
            {
 
-               if(rs.next())
+               while(rs.next())
                {
-
+                   
                    System.out.print("Details for Country: ");
                    String country = rs.getString("Country");
                    System.out.println(country);
@@ -97,30 +104,32 @@ public class Main {
                    int population = rs.getInt("Population");
                    System.out.println(population);
                    System.out.print("GDP: ");
-                   int gdp = rs.getInt("GDP");
+                   double gdp = rs.getDouble("GDP");
                    System.out.println(gdp);
                    System.out.println();
-               }
-               else
-               {
-                   System.out.println("The ID you provided doesn't exit.");
                }
 
 
            }
+           catch (SQLException e)
+           {
+               System.out.println("Unable to execute statement.");
+               e.printStackTrace();
 
-
-
+           }
         }
-
-
+        catch (SQLException e)
+        {
+            System.out.println("Unable to prepare statement.");
+            e.printStackTrace();
+        }
     }
     catch(SQLException e)
     {
 
 
         System.out.println("Unable to connect to the database.");
-
+        e.printStackTrace();
 
     }
 
